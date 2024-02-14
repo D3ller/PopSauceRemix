74a3f8ef5e452f4b5fa9ac6edcf47cf617d45eb7
Ewan6g <corentin.nelhomme@gmail.com>
2024-02-14 11:15:24 +0100

    Passage des elements dans Home

--- Pop/src/views/HomeView.vue
+++ Pop/src/views/HomeView.vue
@@ -1,8 +1,124 @@
 <script setup>
+import socket from "@/socket.js";
+import { RouterLink, RouterView } from 'vue-router'
+
+import {onMounted, onUnmounted, ref} from 'vue';
+import socket from './socket';
+import {io} from "socket.io-client";
+import Navbar from "@/components/Navbar.vue";
+let roomName = ref('');
+let inviteCode = ref('');
+let message = ref('');
+let shareCode = ref('');
+let Privacy = ref(false);
+let players = ref([]);
+let chats = ref([]);
+let publicRooms = ref([]);
+
+onMounted(() => {
+
+  socket.emit('getPublicRooms');
+
+  socket.on('publicRooms', function(publicRoom) {
+    publicRooms.value = publicRoom;
+  })
+
+  socket.on('connect', () => {
+    console.log('Connecté au serveur Socket.IO');
+  });
+
+  socket.on('roomCreated', (room, code) => {
+    alert(`Salon créé: ${room}`)
+    shareCode.value = code;
+  });
+
+  socket.on('joinedRoom', (room) => {
+    alert(`Salon rejoint: ${room}`)
+  });
+
+  socket.on('alreadyInRoom', (room) => {
+    alert(`Vous êtes déjà dans le salon: ${room}`)
+  });
+
+  socket.on('roomJoined', (room) => {
+    alert(`Salon rejoint: ${room}`)
+  });
+
+  socket.on('roomUpdated', (rooms) => {
+    console.log(rooms)
+    players.value = rooms;
+  });
+
+  socket.on('roomClosed', (room) => {
+    alert(`Salon fermé: ${room}`)
+    players.value = [];
+    chats.value = [];
+  })
+
+  socket.on('roomNotFound', (room) => {
+    alert(`Aucun salon avec le code d'invitation: ${room.inviteCode}`)
+  })
+
+  socket.on('chatEnter', (chat) => {
+    console.log(chat);
+    chats.value.push(chat);
+    console.log(chats.value)
+  });
+
+  socket.on('chatNotFound', () => {
+    console.log('Vous ne pouvez pas envoyer de message dans un salon que vous n\'avez pas rejoint');
+  });
+
+  socket.on('roomLeft', (room) => {
+    players.value = [];
+    chats.value = [];
+    shareCode.value = '';
+  })
+
+
+  onUnmounted(() => {
+    socket.disconnect();
+  });
+});
+
+function sendMessage() {
+  socket.emit('chatEnter', message.value);
+  message.value = '';
+}
+
+function togglePrivacy() {
+  console.log(Privacy.value);
+  Privacy.value = !Privacy.value;
+}
 </script>
 
 <template>
   <main>
+    <button @click="socket.emit('message', 'Hello, world!')">Envoyer un message</button>
+    <button @click="socket.emit('createRoom', roomName, Privacy)">Créer un salon</button>
+    <input type="checkbox" checked id="private" @change="togglePrivacy" />
+    <label for="private">{{ Privacy ? 'Privé' : 'Public' }}</label>
 
+    <button @click="socket.emit('leaveRoom')">Quitter le salon</button>
+
+    <div v-for="player in players" :key="player">
+      <router-link :to="`/room/${player}`">{{ player }}</router-link>
+    </div>
+
+    <p>Code d'invitation: {{ shareCode }}</p>
+
+    <input v-model="roomName" placeholder="Nom du salon" />
+    <input v-model="inviteCode" placeholder="Code d'invitation" />
+    <button @click="socket.emit('joinRoom', {inviteCode})">Rejoindre un salon</button>
+
+    <input v-model="message" @keyup.enter="sendMessage" placeholder="Message" />
+
+    <div>
+      <p v-for="chat in chats" :key="chat.id">{{ chat }}</p>
+    </div>
+
+    <div v-for="room in publicRooms" :key="room">
+      <router-link :to="`/room/${room}`">{{ room }}</router-link>
+    </div>
   </main>
 </template>
