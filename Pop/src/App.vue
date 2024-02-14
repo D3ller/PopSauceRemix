3b38caa2be1fb75afeeb974acaa8eb6b98feba27
EmilienRozier <emilienrozier@icloud.com>
2024-02-14 11:12:25 +0100

    suppr components

--- Pop/src/App.vue
+++ Pop/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 
 import {onMounted, onUnmounted, ref} from 'vue';
 import socket from './socket';
