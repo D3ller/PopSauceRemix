181cc333b8ffa658f699fed97adaeae29e46d1c7
Cadet <126462556+Marius0004@users.noreply.github.com>
2024-02-22 14:57:25 +0100

    tableau des scores chat et guest

--- Pop/src/components/room/SideChatAndPlayers.vue
+++ Pop/src/components/room/SideChatAndPlayers.vue
@@ -16,6 +16,7 @@ console.log(props)
           <div id="player_contener">
             <img id="player_contener_picture_player" src="@/assets/images/cirlce_user.png" alt="image_du_player">
             <p id="player_contener_name_player">{{ player.username }}</p>
+            <p>Score: 40</p>
           </div>
         </div>
   </div>
@@ -25,15 +26,14 @@ console.log(props)
       <h3>Chat</h3>
       <div class="chat_container">
         <div class="chat_message">
-          <p>Username</p>
-          <p>Message</p>
-        </div>
+        <p>{{ props.player[0].username }}</p>
       </div>
       <div class="chat_input">
-        <input type="text" placeholder="Ecrire un message" />
-        <button>Envoyer</button>
+        <input class="input" type="text" placeholder="Ecrire un message" />
+        <button><img src="@/assets/images/avion-en-papier.png" alt="icone d'avion en papier"> Envoyer</button>
       </div>
     </div>
+  </div>
 
   </div>
 
@@ -46,6 +46,8 @@ console.log(props)
   height: 100vh;
   display: grid;
   grid-template-rows: 1fr 1fr;
+  color: #D9D9D9;
+  font-family: Raleway, sans-serif;
 }
 
 .side_players h3 {
@@ -64,13 +66,73 @@ console.log(props)
   align-items: center;
   margin: 10px;
   height: 50px;
+  color: #222222;
 }
+
 #player_contener_name_player{
- font-family: 'Courier New', Courier, monospace;
  margin: 25px;
 }
 #player_contener_picture_player{
   width: 15%;
   margin: 10px;
+}
+.side_chat{
+ display: flex;
+ flex-direction: column;
+    justify-content: space-between;
+}
+.side_chat h3 {
+  margin-left: 15px;
+}
+.chat_container{
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-evenly;
+  height: 100%;
+  margin-bottom: 10px;
+}
+.chat_input{
+  display: flex;
+}
+.input {
+  width: 100%;
+  max-width: 180px;
+  height: 40px;
+  padding: 6px;
+ 
+  border: 1.5px solid lightgrey;
+  outline: none;
+  transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+  box-shadow: 0px 0px 20px -18px;
+}
+
+.input:hover {
+  border: 2px solid lightgrey;
+  box-shadow: 0px 0px 20px -17px;
+}
+
+.input:active {
+  transform: scale(0.95);
+}
+
+.input:focus {
+  border: 2px solid grey;
+}
+button {
+  font-family: inherit;
+    font-size: 12px;
+    background: royalblue;
+    color: white;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    border: none;
+    overflow: hidden;
+    transition: all 0.2s;
+    cursor: pointer;
+}
+button img{
+  width: 20px;
+  margin-right: 5px;
 }
 </style>
