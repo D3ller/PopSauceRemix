7a07b42bb330cb6bcb087b999e307f8715beacf2
EmilienRozier <emilienrozier@icloud.com>
2024-03-12 10:44:58 +0100

    client create account / docker compose volume

--- client/src/views/account/CreateAccount.vue
+++ client/src/views/account/CreateAccount.vue
@@ -3,6 +3,7 @@ import router from '@/router';
 import { ref } from 'vue';
 
 // import { ref } from 'vue';
+import axios from 'axios';
 
     // const user = ref(true);
 
@@ -10,31 +11,66 @@ import { ref } from 'vue';
     const username = ref('')
     const mdp = ref('')
     const confirm_mdp = ref('')
+    const alreadyExist = ref(false)
 
-    const submit = () => {
+    const createAccount = () => {
         const user = {
             mail: mail.value,
             username: username.value,
             mdp: mdp.value,
-            confirm_mdp: confirm_mdp.value,
         }
-        console.log(user)
+    
+        if(mdp.value != confirm_mdp.value) {
+            console.log('pas les mêmes mdps')
+            return;
+        }
 
-        if(user) {
-            router.push('/account')
-        }
+        axios.get('http://localhost:8080/api/users')
+            .then(res => {
+                const userExist = res.data["hydra:member"].find(u => u.email === user.mail)
+                if(userExist) {
+                    alreadyExist.value = true
+                    console.log('user already exist')
+                } else {
+                    axios.post('http://localhost:8080/api/users', {
+                        "pseudo": user.username,
+                        "mdp": user.mdp,
+                        "email": user.mail,
+                    })
+
+                    // private key a passer dans un .env plus tard 
+
+                    const privateKey = 'privatekey'
+                    var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+                        console.log(token);
+                    });
+
+                    document.cookie = `Token=${token};Max-Age=60`
+
+                    router.push('/account')
+                }
+            })
+    }
+
+    const test = () => {
+        const privateKey = 'privatekey'
+            var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+            console.log(token);
+        });
     }
 
 </script>
 
 <template>
-    <form @submit.prevent="submit">
+    <form @submit.prevent="createAccount">
         <input type="mail" placeholder="email" v-model="mail">
         <input type="username" placeholder="username" v-model="username">
         <input type="password" placeholder="mdp" v-model="mdp">
         <input type="password" placeholder="confirm mdp" v-model="confirm_mdp">
         <button>Créer compte</button>
     </form>
+
+    <button @click="test">test</button>
 </template>
 
 <style scoped>
