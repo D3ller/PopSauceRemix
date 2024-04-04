3a7e443ea251e2c4231f6afb3c22c19ba7cb9ef2
RichardQuin <126667403+RichardQuin@users.noreply.github.com>
2024-04-04 15:05:05 +0200

    mention legale

--- client/src/views/legals/mention-legals.vue
+++ client/src/views/legals/mention-legals.vue
@@ -4,6 +4,56 @@
 
 <template>
 
+<div class="mentions">
+<p>
+ 
+<h1>Mentions légales</h1>
+
+<h2>Éditeur du site :</h2>
+
+Question pour un buisson
+9 Rue Quebec, 10430 Rosières-prés-Troyes
+0647213274
+QuestionPourUnBusson@gmail.com
+
+**Directeur de la publication :**
+
+Richard Quin
+
+<h2>Hébergeur :</h2>
+
+OVH
+ 2 rue Kellermann - 59100 Roubaix - France
+537 407 926
+
+<h2>Propriété intellectuelle :</h2>
+
+Le contenu de ce site (textes, images, vidéos, etc.) est protégé par le droit d'auteur. Toute reproduction ou utilisation non autorisée du contenu est interdite.
+
+<h2>Collecte de données personnelles :</h2>
+
+Les informations personnelles collectées via ce site sont utilisées dans le seul but de fournir les services proposés (notamment les quiz sur l'écologie) et ne sont en aucun cas transmises à des tiers sans consentement préalable.
+
+<h2>Cookies :</h2>
+
+Ce site utilise des cookies pour améliorer l'expérience de l'utilisateur. En naviguant sur ce site, vous acceptez l'utilisation de cookies conformément à notre politique de confidentialité.
+
+**Liens externes :**
+
+Ce site peut contenir des liens vers des sites tiers. Nous ne sommes pas responsables du contenu ou des pratiques de confidentialité de ces sites.
+
+<h2>Contact :</h2>
+
+Pour toute question ou réclamation concernant ce site, veuillez nous contacter à l'adresse e-mail suivante : [Adresse e-mail de contact]
+
+<h2>Modification des mentions légales :</h2>
+
+Nous nous réservons le droit de modifier les présentes mentions légales à tout moment. Nous vous conseillons donc de consulter cette page régulièrement pour vous tenir informé des éventuelles mises à jour.
+
+<h2>Date de dernière mise à jour </h2> 04/04/2024
+
+</p>
+</div>
 </template>
 
 <style scoped lang="scss">
