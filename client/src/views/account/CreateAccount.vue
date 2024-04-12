aae59d9b20eafac2385832826488df1c3e808b48
EmilienRozier <emilien.rozier@etudiant.univ-reims.fr>
2024-04-12 15:26:05 +0200

    login & register

--- client/src/views/account/CreateAccount.vue
+++ client/src/views/account/CreateAccount.vue
@@ -14,13 +14,75 @@ import Trans from "@/i18n/translation.js";
     password: "",
   })
 
-const store = new Store()
+// const store = new Store()
+
+function getToken() {
+  var cookieName = "token"
+  var cookieArray = document.cookie.split(';');
+  for (var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while (cookie.charAt(0) == ' ') {
+      cookie = cookie.substring(1);
+    }
+
+    if (cookie.indexOf(cookieName + '=') == 0) {
+      return cookie.substring(cookieName.length + 1, cookie.length);
+    }
+  }
+}
+
+function handleLogin(form) {
+  axios.post('http://localhost:8080/api/login_check', {
+    username: form.username,
+    password: form.password
+  })
+      .then(async (res) => {
+        console.log(res)
+        if (res.data.token) {
+          var dateExpiration = new Date();
+          dateExpiration.setTime(dateExpiration.getTime() + (1 * 60 * 60 * 1000));
+          console.log(dateExpiration)
+          var dateUTC = dateExpiration.toUTCString();
+          document.cookie = `token=${res.data.token};expires=${dateUTC};path=/`;
+
+          let token = await getToken();
+          console.log(token);
+
+          const userResponse = await axios.get('http://localhost:8080/api/me', {
+            headers: {Authorization: `Bearer ${token}`}
+          });
+
+          localStorage.setItem('user', JSON.stringify(userResponse.data.user))
+
+          await router.push('/fr/account');
+        }
+      })
+}
+
+function handleRegister(user) {
+  try {
+    axios.post('http://localhost:8080/api/users', user)
+        .then(async (res) => {
+          if (res.status === 201) {
+            console.log(res.data)
+            const login = {
+              username: user.email,
+              password: user.password
+            }
+            await handleLogin(login)
+            // router.push('/fr/account')
+          }
+        })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 </script>
 
 <template>
   <div class="account_div">
-    <form class="account_form" @submit.prevent="store.handleRegister(user)">
+    <form class="account_form" @submit.prevent="handleRegister(user)">
 
       <h1 class="account_title">Crée un compte</h1>
       <hr class="bar">
